8372bcf3fec10bc9f7da17b405157cb8753fe7c3
DevSebastian1 <sebastian.espinosa@upr.edu>
2024-10-01 18:05:59 -0400

    Barebones results page, return home button

---
+++ src/ui/static/script/return-main.js
@@ -0,0 +1,5 @@
+document.addEventListener('DOMContentLoaded', function () {
+    document.getElementById('returnButton').addEventListener('click', function() {
+        window.location.href = '/';
+    });
+});
