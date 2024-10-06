861c1160eb2edd7788b40e78365e69df4fd34ac5
DevSebastian <sebastian.espinosa@upr.edu>
2024-10-06 15:44:32 -0400

    Shows results from the models prediction in a new page

--- src/ui/app.py
+++ src/ui/app.py
@@ -8,42 +8,69 @@ app = Flask(__name__)
 # load model
 model = tf.keras.models.load_model("../../best_model/mobilenetv2_best_f1score_fold_1.h5")
 
+# Establish Labels
+classified_as = ['Monkeypox', 'Not Monkeypox']
+
 @app.route('/')
 def landing():
     return render_template('landing.html')
 
 
-@app.route('/upload_success')
+@app.route('/upload/success')
 def upload_success():
-    return render_template('upload-success.html')
+    result = request.args.get('result')  
+    probability = request.args.get('probability')
+
+    return render_template('upload-success.html', result = result, probability = probability)
+
+
+@app.route('/upload/failed')
+def upload_failed():
+    return render_template('upload-failed.html')
 
 
 @app.route('/upload', methods=['POST'])
 def upload():
-    # some error handling
-    if 'file' not in request.files:
-        return "No file part"
-    file = request.files['file']
-    
-    if file.filename == '':
-        return "No selected file"
-    
-    if file:
-        # Convert file to image
-        img = Image.open(file.stream).resize((224, 224))
-        img = np.reshape(np.array(img), [224, 224, 3])
-        img = img[np.newaxis, ...]
+    wrongFile = False
+    try: 
+        # some error handling
+        if 'file' not in request.files:
+            wrongFile = True
+        file = request.files['file']
+        
+        if file.filename == '':
+            return "No selected file"
+        
+        if file:
+            # Convert file to image
+            img = Image.open(file.stream).resize((224, 224))
+            img = np.reshape(np.array(img), [224, 224, 3])
+            img = img[np.newaxis, ...]
 
-        img_to_tensor = tf.convert_to_tensor(img)
+            img_to_tensor = tf.convert_to_tensor(img)
 
-        # Run prediction
-        output = model.predict(img_to_tensor)
-        # output = model.predict_classes(img_to_tensor)
+            # Run prediction
+            output = model.predict(img_to_tensor)
 
-        print(output)
-        print(output[0][0])
-        # Redirect to success page if model was able to process it correctly
-        return redirect(url_for('upload_success'))
+            # Monkeypox = 0 and Other = 1
+            classes = np.argmax(output, axis = 1)
+
+            # Probability for the result
+            score = tf.nn.sigmoid(output[0])
+
+            # Classify prediction and score
+            class_ = classified_as[classes[0]]
+            probability = score[classes[0]].numpy()
+            percentage_value = round(probability * 100, 2)
+
+            # Redirect to success page if model was able to process it correctly
+            # return redirect(url_for('upload_success'))
+            return redirect(url_for('upload_success', result=class_, probability=percentage_value))
+    except:
+        if(wrongFile): 
+            print('Wrong file, maybe add as a notification and send back to landing')
+            return redirect(url_for('/'))
+        return redirect(url_for('upload_failed'))
 
 if __name__ == "__main__":
     app.run(debug=True)
